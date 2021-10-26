fa43d69b9c8e8aff36ff3dda29975b8e6534e315
qiuxin2012 <qiuxin2012cs@gmail.com>
2021-10-26 15:09:48 +0800

    add cluster mode

--- docker/bigdl-k8s/k8s-integration-tests.sh
+++ docker/bigdl-k8s/k8s-integration-tests.sh
@@ -9,7 +9,7 @@ ${SPARK_HOME}/bin/spark-submit \
   --conf spark.driver.host=${RUNTIME_DRIVER_HOST} \
   --conf spark.driver.port=${RUNTIME_DRIVER_PORT} \
   --conf spark.kubernetes.authenticate.driver.serviceAccountName=${RUNTIME_K8S_SERVICE_ACCOUNT} \
-  --name bigdl2-cifar10 \
+  --name bigdl2-fasion_mnist \
   --conf spark.kubernetes.container.image=${RUNTIME_K8S_SPARK_IMAGE} \
   --conf spark.executor.instances=${RUNTIME_EXECUTOR_INSTANCES} \
   --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.options.claimName=${RUNTIME_PERSISTENT_VOLUME_CLAIM} \
@@ -30,7 +30,7 @@ ${SPARK_HOME}/bin/spark-submit \
   --driver-cores ${RUNTIME_DRIVER_CORES} \
   --driver-memory ${RUNTIME_DRIVER_MEMORY} \
   --properties-file ${BIGDL_HOME}/conf/spark-bigdl.conf \
-  --py-files local://${BIGDL_HOME}/python/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-serving-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local:///opt/bigdl-0.14.0-SNAPSHOT/examples/orca/learn/pytorch/cifar10/cifar10.py \
+  --py-files local://${BIGDL_HOME}/python/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-serving-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local:///opt/bigdl-0.14.0-SNAPSHOT/examples/orca/learn/pytorch/fasion_mnist/fasion_mnist.py \
   --conf spark.driver.extraJavaOptions=-Dderby.stream.error.file=/tmp \
   --conf spark.sql.catalogImplementation='in-memory' \
   --conf spark.driver.extraClassPath=local://${BIGDL_HOME}/jars/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar \
@@ -40,49 +40,49 @@ ${SPARK_HOME}/bin/spark-submit \
 endtime=`date +'%Y-%m-%d %H:%M:%S'`
 start_seconds=$(date --date="$starttime" +%s);
 end_seconds=$(date --date="$endtime" +%s);
-echo "################## end cifar10.py client "
+echo "################## end fasion_mnist.py client "
 echo "run time is： "$((end_seconds-start_seconds))"s"
   
-#echo "################## start cifar10.py  cluster "
-#starttime=`date +'%Y-%m-%d %H:%M:%S'`
-###  cifar10.py cluster
-#${SPARK_HOME}/bin/spark-submit \
-#  --master ${RUNTIME_SPARK_MASTER} \
-#  --deploy-mode cluster \
-#  --conf spark.kubernetes.authenticate.driver.serviceAccountName=${RUNTIME_K8S_SERVICE_ACCOUNT} \
-#  --name bigdl2-cifar10 \
-#  --conf spark.kubernetes.container.image=${RUNTIME_K8S_SPARK_IMAGE} \
-#  --conf spark.executor.instances=${RUNTIME_EXECUTOR_INSTANCES} \
-#  --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.options.claimName=${RUNTIME_PERSISTENT_VOLUME_CLAIM} \
-#  --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.mount.path=/tmp \
-#  --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.options.claimName=${RUNTIME_PERSISTENT_VOLUME_CLAIM} \
-#  --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.mount.path=/tmp \
-#  --conf spark.kubernetes.driver.label.az=true \
-#  --conf spark.kubernetes.executor.label.az=true \
-#  --conf spark.kubernetes.node.selector.spark=true \
-#  --conf spark.kubernetes.driverEnv.http_proxy=${http_proxy} \
-#  --conf spark.kubernetes.driverEnv.https_proxy=${https_proxy} \
-#  --conf spark.kubernetes.executorEnv.http_proxy=${http_proxy} \
-#  --conf spark.kubernetes.executorEnv.https_proxy=${https_proxy} \
-#  --conf spark.kubernetes.container.image.pullPolicy=Always \
-#  --executor-cores ${RUNTIME_EXECUTOR_CORES} \
-#  --executor-memory ${RUNTIME_EXECUTOR_MEMORY} \
-#  --total-executor-cores ${RUNTIME_TOTAL_EXECUTOR_CORES} \
-#  --driver-cores ${RUNTIME_DRIVER_CORES} \
-#  --driver-memory ${RUNTIME_DRIVER_MEMORY} \
-#  --properties-file ${BIGDL_HOME}/conf/spark-bigdl.conf \
-#  --py-files local://${BIGDL_HOME}/python/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-serving-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local:///opt/bigdl-0.14.0-SNAPSHOT/examples/orca/learn/pytorch/cifar10/cifar10.py \
-#  --conf spark.driver.extraJavaOptions=-Dderby.stream.error.file=/tmp \
-#  --conf spark.sql.catalogImplementation='in-memory' \
-#  --conf spark.driver.extraClassPath=local://${BIGDL_HOME}/jars/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar \
-#  --conf spark.executor.extraClassPath=local://${BIGDL_HOME}/jars/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar \
-#  local:///opt/bigdl-0.14.0-SNAPSHOT/examples/orca/learn/pytorch/cifar10/cifar10.py \
-#  --backend torch_distributed
-#endtime=`date +'%Y-%m-%d %H:%M:%S'`
-#start_seconds=$(date --date="$starttime" +%s);
-#end_seconds=$(date --date="$endtime" +%s);
-#echo "################## end cifar10.py  cluster "
-#echo "run time is： "$((end_seconds-start_seconds))"s"
+echo "################## start fasion_mnist cluster "
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+##  fasion_mnist.py cluster
+${SPARK_HOME}/bin/spark-submit \
+  --master ${RUNTIME_SPARK_MASTER} \
+  --deploy-mode cluster \
+  --conf spark.kubernetes.authenticate.driver.serviceAccountName=${RUNTIME_K8S_SERVICE_ACCOUNT} \
+  --name bigdl2-fasion-mnist \
+  --conf spark.kubernetes.container.image=${RUNTIME_K8S_SPARK_IMAGE} \
+  --conf spark.executor.instances=${RUNTIME_EXECUTOR_INSTANCES} \
+  --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.options.claimName=${RUNTIME_PERSISTENT_VOLUME_CLAIM} \
+  --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.mount.path=/tmp \
+  --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.options.claimName=${RUNTIME_PERSISTENT_VOLUME_CLAIM} \
+  --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.${RUNTIME_PERSISTENT_VOLUME_CLAIM}.mount.path=/tmp \
+  --conf spark.kubernetes.driver.label.az=true \
+  --conf spark.kubernetes.executor.label.az=true \
+  --conf spark.kubernetes.node.selector.spark=true \
+  --conf spark.kubernetes.driverEnv.http_proxy=${http_proxy} \
+  --conf spark.kubernetes.driverEnv.https_proxy=${https_proxy} \
+  --conf spark.kubernetes.executorEnv.http_proxy=${http_proxy} \
+  --conf spark.kubernetes.executorEnv.https_proxy=${https_proxy} \
+  --conf spark.kubernetes.container.image.pullPolicy=Always \
+  --executor-cores ${RUNTIME_EXECUTOR_CORES} \
+  --executor-memory ${RUNTIME_EXECUTOR_MEMORY} \
+  --total-executor-cores ${RUNTIME_TOTAL_EXECUTOR_CORES} \
+  --driver-cores ${RUNTIME_DRIVER_CORES} \
+  --driver-memory ${RUNTIME_DRIVER_MEMORY} \
+  --properties-file ${BIGDL_HOME}/conf/spark-bigdl.conf \
+  --py-files local://${BIGDL_HOME}/python/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-serving-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local://${BIGDL_HOME}/python/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-python-api.zip,local:///opt/bigdl-0.14.0-SNAPSHOT/examples/orca/learn/pytorch/fasion_mnist/fasion_mnist.py \
+  --conf spark.driver.extraJavaOptions=-Dderby.stream.error.file=/tmp \
+  --conf spark.sql.catalogImplementation='in-memory' \
+  --conf spark.driver.extraClassPath=local://${BIGDL_HOME}/jars/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar \
+  --conf spark.executor.extraClassPath=local://${BIGDL_HOME}/jars/bigdl-orca-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-dllib-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar:local://${BIGDL_HOME}/jars/bigdl-friesian-spark_${SPARK_VERSION}-${BIGDL_VERSION}-jar-with-dependencies.jar \
+  local:///opt/bigdl-0.14.0-SNAPSHOT/examples/orca/learn/pytorch/fasion_mnist/fasion_mnist.py \
+  --backend torch_distributed
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s);
+end_seconds=$(date --date="$endtime" +%s);
+echo "################## end fasion_mnist.py  cluster "
+echo "run time is： "$((end_seconds-start_seconds))"s"
 
 #echo "################## start autoestimator_pytorch.py  client "
 #starttime=`date +'%Y-%m-%d %H:%M:%S'`

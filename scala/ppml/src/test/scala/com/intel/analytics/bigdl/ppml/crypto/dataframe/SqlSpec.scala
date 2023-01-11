c68f263926d5ad6a1d880980d8eff16e4bd0b164
qiuxin2012 <qiuxin2012cs@gmail.com>
2023-01-11 16:42:04 +0800

    sql

---
+++ scala/ppml/src/test/scala/com/intel/analytics/bigdl/ppml/crypto/dataframe/SqlSpec.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 The BigDL Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intel.analytics.bigdl.ppml.crypto.dataframe
+
+import com.intel.analytics.bigdl.dllib.utils.LoggerFilter
+import com.intel.analytics.bigdl.ppml.PPMLContext
+import com.intel.analytics.bigdl.ppml.crypto._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+import java.io.{File, FileWriter}
+
+class SqlSpec extends DataFrameHelper {
+  LoggerFilter.redirectSparkInfoLogs()
+
+  val (plainFileName, encryptFileName, data, dataKeyPlaintext) = generateCsvData()
+
+  val ppmlArgs = Map(
+      "spark.bigdl.kms.simple.id" -> appid,
+      "spark.bigdl.kms.simple.key" -> apikey,
+      "spark.bigdl.kms.key.primary" -> primaryKeyPath,
+      "spark.bigdl.kms.key.data" -> dataKeyPath
+  )
+  val sparkConf = new SparkConf().setMaster("local[4]")
+    .set("spark.sql.catalogImplementation", "hive")
+  val sc = PPMLContext.initPPMLContext(sparkConf, "SimpleQuery", ppmlArgs)
+
+  "sparkSession.read" should "work" in {
+    val sparkSession: SparkSession = SparkSession
+      .builder()
+      .appName("Spark Hive Example")
+      .enableHiveSupport()
+      .getOrCreate()
+//    sparkSession.sql("create database abc;")
+    sparkSession.sql("use abc;")
+//    sparkSession.sql("create table People(name string, age int, job string)" +
+//      " row format delimited fields terminated by ',';")
+//    sparkSession.sql(s"LOAD DATA LOCAL INPATH '${plainFileName}' OVERWRITE INTO TABLE People;")
+    val r = sparkSession.sql(s"select count(*) from People")
+
+    println(r.take(1))
+
+
+  }
+
+}
+

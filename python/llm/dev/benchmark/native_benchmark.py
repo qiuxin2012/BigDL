c6ce5abcd59b28f47f4982d4dc7165633c477810
qiuxin2012 <qiuxin2012cs@gmail.com>
2023-08-01 11:04:40 +0800

    update transformer benchmark

--- python/llm/dev/benchmark/native_benchmark.py
+++ python/llm/dev/benchmark/native_benchmark.py
@@ -9,41 +9,48 @@ from torch.profiler import profile, record_function, ProfilerActivity
 import psutil
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Transformer INT4 example')
+    parser = argparse.ArgumentParser(description='Native INT4 example')
     parser.add_argument('--repo-id', type=str, required=True,
                         help='The huggingface repo id for the large language model to be downloaded'
-                             ', or the path to the huggingface checkpoint folder')
+                             ', or the path to the huggingface checkpoint folder.')
+    parser.add_argument('--save-path', type=str, required=True,
+                        help='path to save the model.')
     parser.add_argument('--local-model-hub', type=str,
                         help='the local caching hub dir.')
     parser.add_argument('--prompt-len', type=int, default=32,
-                        help='prompt length to infer')
+                        help='prompt length to infer.')
     parser.add_argument('--infer-times', type=int, default=3,
-                        help='inference times for tests')
+                        help='inference times for tests.')
+    parser.add_argument('--model-family', type=str, default="llama",
+                        help='The model family of the loaded model.')
     args = parser.parse_args()
 
     repo_id = args.repo_id
+    save_path = args.save_path
+    model_family = args.model_family
+    n_ctx = args.prompt_len + 32
+
     if args.local_model_hub:
         repo_model_name = repo_id.split("/")[1]
-        model_path = args.local_model_hub + "/" + repo_model_name
+        model_path = args.local_model_hub + os.path.sep + repo_model_name
     else:
         model_path = repo_id
     infer_times = args.infer_times
+    from bigdl.llm import llm_convert
+    bigdl_llm_path = llm_convert(model=model_path,
+        outfile=save_path, outtype='int4', model_family=model_family)
+    print(f"savepath={save_path}, llmconv={bigdl_llm_path}")
+
+#load the converted model
+    from bigdl.llm.transformers import BigdlNativeForCausalLM
+    model = BigdlNativeForCausalLM.from_pretrained(bigdl_llm_path, n_ctx=n_ctx)
 
     st = time.time()
     # TODO: auto detect using config.json
-    if repo_id in ['THUDM/chatglm-6b', 'THUDM/chatglm2-6b']:
-        model = AutoModel.from_pretrained(model_path, load_in_4bit=True, trust_remote_code=True, torch_dtype='auto')
-        tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    else:
-        model = AutoModelForCausalLM.from_pretrained(model_path, load_in_4bit=True)
-        end = time.time()
-        print(">> loading of model costs {}s".format(end-st))
-        tokenizer = LlamaTokenizer.from_pretrained(model_path)
+    # tokenizer = LlamaTokenizer.from_pretrained(model_path)
 
     end = time.time()
     print(">> loading and conversion of model costs {}s".format(end-st))
-
-    model = BenchmarkWrapper(model)
 
     input_str = open(f"prompt/{args.prompt_len}.txt", 'r').read()
     input_str = input_str.rstrip()
@@ -52,14 +59,15 @@ if __name__ == '__main__':
     print(input_str)
 
     for i in range(infer_times):
-        input_ids = tokenizer.encode(input_str, return_tensors="pt")
+        input_ids = model.tokenize(input_str)
         # As different tokenizer has different encodings,
         # slice the input_ids to ensure the prompt length is required length.
-        input_ids = input_ids[:, :args.prompt_len]
-        print("input length is : ", input_ids.shape[1])
+        print("Origin input_ids is", len(input_ids))
+        input_ids = input_ids[:args.prompt_len]
+        print("Sliced input length is : ", len(input_ids))
         st = time.time()
-        output_ids = model.generate(input_ids, do_sample=False, max_new_tokens=32)
-        output = tokenizer.batch_decode(output_ids)
-        true_input = tokenizer.batch_decode(input_ids)
+        output_ids = model.generate(input_ids, max_new_tokens=32)
+        output = model.batch_decode(output_ids)
+        true_input = model.batch_decode(input_ids)
         print(true_input + output[0])
         end = time.time()
